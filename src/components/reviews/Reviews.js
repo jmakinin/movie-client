2375190cc1857246c4d6f60d812885e92017eb11
jmakinin <36607363+jmakinin@users.noreply.github.com>
2023-06-17 10:26:10 +0000

    review posting and updating reviews for movies

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,89 @@
+import { useEffect, useRef } from "react";
+import api from '../../api/aviosConfig';
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+
+const Reviews = ({ getMovieData, movies, reviews, setReviews }) => {
+
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(() => {
+        getMovieData(movieId)
+    }, [])
+
+    const addReview = async (e) => {
+        e.preventDefault();
+
+        const rev = revText.current;
+
+        try {
+
+            const response = await api.post("/reviews", { reviewBody: rev.value, imdbId: movieId });
+
+            const updatedReviews = [...reviews, { body: rev.value }]
+
+            rev.value = "";
+
+            setReviews(updatedReviews)
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col><h3>Reviews</h3></Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={movies?.poster} alt="" />
+                </Col>
+                <Col>
+                    {
+                        <>
+                            <Row>
+                                <Col>
+                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <hr />
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    {
+                        reviews?.map((r) => {
+                            return (
+                                <>
+                                    <Row>
+                                        <Col>{r.body}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <hr />
+                                        </Col>
+                                    </Row>
+                                </>
+                            )
+                        })
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <hr />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Reviews
